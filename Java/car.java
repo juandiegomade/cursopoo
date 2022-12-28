76b4b79af9db2667981801d53a003cfec5a25b93
juandiegomade <juandiegomade@gmail.com>
2022-12-27 22:00:55 -0600

    First commit wiht OOP course files

---
+++ Java/car.java
@@ -0,0 +1,8 @@
+package Java;
+
+ class car {
+    Integer id;
+    String license;
+    String driver;
+    Integer passengers;    
+}
---
+++ Java/route.java
@@ -0,0 +1,9 @@
+package Java;
+
+import java.util.ArrayList;
+
+class route {
+    Integer id;
+    ArrayList<Double> start;
+    ArrayList<Double> end;    
+}
