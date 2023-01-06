1222764964a05c09e20e5aacae5f3a77210fe432
juandiegomade <juandiegomade@gmail.com>
2023-01-05 22:05:43 -0600

    inheritance

--- PHP/index.php
+++ PHP/index.php
@@ -6,5 +6,6 @@ require_once('account.php');
 
 $uberX = new UberX("CVB123", new Account("Andres Herrera", "AND456", "Chevrolet", "Spark"))
 $uberX->PrintDataCar();
-$car = new Car("AW456", new Account("Andres Herrera", "AMS123"));
-$car->PrintDataCar();
+
+$uberPool = new UberPool("TYU567", new Account("Andrea Ferran", "ANDA765", "Dodge", "Attitude")
+$uberPool->printDataCar();
