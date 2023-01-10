ba21a2ae3070e81b803d25ab73f95f11c3c3b023
juandiegomade <juandiegomade@gmail.com>
2023-01-09 21:55:52 -0600

    Creating classes in Python and JS

--- Java/Main.java
+++ Java/Main.java
@@ -3,12 +3,12 @@ package Java;
 class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
-        Car car = new Car("AMQ123", new Account("Andres Herrera", "AND123"));
-        car.passengers = 4;
-        car.printDataCar();
+        UberX uberX = new UberX("AMQ123", new Account("Andres Herrera", "AND123"), "Chevrolet", "Sonic");
+        uberX.passengers = 4;
+        uberX.printDataCar();
 
-        Car car2 = new Car("QWE567", new Account("Andrea Herrera", "ANDA876"));
-        car2.passengers = 3;
-        car2.printDataCar();
+        // Car car2 = new Car("QWE567", new Account("Andrea Herrera", "ANDA876"));
+        // car2.passengers = 3;
+        // car2.printDataCar();
     }
 }
